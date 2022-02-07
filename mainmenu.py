5628c2dc194fffa60c667e1c5478bcc19c07901d
AmeeMadhani <96717139+AmeeMadhani@users.noreply.github.com>
2022-02-07 19:30:39 +0530

    Update mainmenu.py

--- mainmenu.py
+++ mainmenu.py
@@ -21,12 +21,22 @@ class MainMenu:
         self.Font4 = ("Montserrat", 20) 
 
 
+
         self.mainlabel = Label(self.root, text="SELF DRIVING CAR SIMULATION", font = self.Font3, fg="#2e2d2d", bg = "#e4e4e4", padx = 150, pady = 20 )
         self.mainlabel.pack(pady=50, padx =50)
 
 
-        self.button_ManualMode = Button(self.root, text="Manual Mode", command = self.Manual,fg ="#5fcca6", bg = "#2e2d2d", font=self.Font2)
-        self.button_AIMode = Button(self.root, text="AI Mode", command =self.AI,fg ="#5fcca6", bg = "#2e2d2d", font=self.Font2 )
+        def combine_funcs(*funcs):
+            def combined_func(*args, **kwargs):
+                    for f in funcs:
+                            f(*args, **kwargs)
+            return combined_func
+
+        allm = lambda: [self.Manual(), self.show()]
+        alla = lambda: [self.AI(), self.show1()]
+        
+        self.button_ManualMode = Button(self.root, text="Manual Mode", command = allm ,fg ="#5fcca6", bg = "#2e2d2d", font=self.Font2)
+        self.button_AIMode = Button(self.root, text="AI Mode", command =alla,fg ="#5fcca6", bg = "#2e2d2d", font=self.Font2 )
         self.button_PaintMode = Button(self.root, text="Paint Mode", command=self.PaintMode, fg ="#5fcca6", bg = "#2e2d2d", font=self.Font2, padx=170)
         self.button_Quit = Button(self.root, text = "Quit", command=self.root.destroy, fg ="#2e2d2d", bg = "#e4e4e4", font=self.Font2i,)
 
@@ -38,7 +48,7 @@ class MainMenu:
 
 
         self.root.mainloop()
-    
+
 
     def Manual(self):
             self.picktrack()
@@ -55,11 +65,42 @@ class MainMenu:
             drawing_window.Paint()
         
     def picktrack(self):
+        #     self.options = ['map01.png', 'map02.png', 'map03.png', 'testmap.png']
+        #     self.master = Tk()
+        #     self.master.geometry("300x100")
+        #     self.head = Label(self.master, text = "choose the map")
+        #     self.head.pack()
+        #     self.variable = StringVar(self.master)
+        #     self.variable.set(self.options[0])
+        #     def ok(event):
+        #         if self.variable == "True":
+        #                 self.variable.set(self.variable.get())
+        #         else:
+        #                 pass
+        #     self.trackname = OptionMenu(self.master, self.variable, *self.options)
+        #     self.trackname.pack()
+         
             self.trackname = simpledialog.askstring("Track Select", "Please enter a track name for the car to drive on \n(without any extensions)") + '.png'
+
+    def show1(self):
+            self.root = Tk()
+            self.root.geometry("200x100")
+            self.root.title("statistics")
+           
+            self.Distance = Label(self.root, text = f"Distance: {''}")
+            self.Deaths = Label(self.root, text = f"Deaths: {''}")
+            self.Time1 = Label(self.root, text = f"Time: {''}")
+            self.Speed = Label(self.root, text = f"Speed: {''}")
+
+            self.Distance.pack()
+            self.Deaths.pack()
+            self.Time1.pack()
+            self.Speed.pack()
             
+            self.root.mainloop()  
+
+    
 
 if __name__ == '__main__':
     MainMenu()
-        
-
-        
+       
